53d4a3f05d4388fbf9402008131493c93a94fba0
yancfgithub <374741622@qq.com>
2021-06-06 23:42:16 +0800

    [UPDATE]day01工程结构搭建，注册中心搭建

---
+++ changgou-parent/changgou-eureka/src/main/java/com/chaofan/EurekaApplication.java
@@ -0,0 +1,27 @@
+package com.chaofan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yan
+ * @create 2021 - 06 - 06 23:35
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
+
+/*@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}*/
